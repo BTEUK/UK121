58b8c2a2b5b02697fe632e65f6775f50f7bd2d20
ELgamerETS2 <laurens.wolfert@gmail.com>
2021-10-19 13:15:38 +0100

    Added comments to EarthBiomeSource

--- src/main/java/net/bteuk/uk121/world/gen/biome/EarthBiomeSource.java
+++ src/main/java/net/bteuk/uk121/world/gen/biome/EarthBiomeSource.java
@@ -15,6 +15,7 @@ import java.util.stream.Collectors;
 
 public class EarthBiomeSource extends BiomeSource {
 
+    //Creates the CODEC for EarthBiomeSource.
     public static final Codec<EarthBiomeSource> CODEC = RecordCodecBuilder.create((instance) ->
             instance.group(RegistryLookupCodec.of(Registry.BIOME_KEY).forGetter((biomeSource) ->
                     biomeSource.BIOME_REGISTRY), Codec.intRange(1, 20).fieldOf("biome_size").orElse(2).forGetter((biomeSource) ->
@@ -26,6 +27,11 @@ public class EarthBiomeSource extends BiomeSource {
     private final long seed;
     private final int biomeSize;
 
+    /*
+    Constructor.
+    Sets Registry<Biome> biomeRegistry with all existing biomes in the game.
+    int BiomeSize and long seed are not applicable for this world generation type, however they are parameters nontheless.
+     */
     public EarthBiomeSource(Registry<Biome> biomeRegistry, int biomeSize, long seed) {
         super(biomeRegistry.getEntries().stream()
                 .filter(entry -> entry.getKey().getValue().getNamespace().equals(UK121.MOD_ID))
@@ -47,14 +53,20 @@ public class EarthBiomeSource extends BiomeSource {
         return new EarthBiomeSource(this.BIOME_REGISTRY, this.biomeSize, seed);
     }
 
-    //New Version of the code.
+    /*
+    Selects the biome for a specific x,z coordinate, y is not taken into account.
+    To add y as a parameter simply edit the method parameters.
+    Any methods that call this method will also need to be adjusted then.
+     */
     public Biome sample(Registry<Biome> dynamicBiomeRegistry, int x, int z) {
 
-        // Spawn ocean if we can't resolve the biome from the layers.
+        //No biome selection process has been added currently, defaults to plains.
         RegistryKey<Biome> backupBiomeKey = BiomeKeys.PLAINS;
         return dynamicBiomeRegistry.get(backupBiomeKey);
     }
 
+    //Overwrites the default biome generation.
+    //Calls sample() to select the biome to return.
     @Override
     public Biome getBiomeForNoiseGen(int x, int y, int z) {
         return this.sample(this.BIOME_REGISTRY, x, z);
