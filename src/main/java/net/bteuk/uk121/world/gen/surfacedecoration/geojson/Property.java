008f8f5d1da68c4a82beb28e12fef03eac8b4ae8
Bluecarpet <76963316+george112n@users.noreply.github.com>
2022-01-16 13:53:45 +0000

    Delete src/main/java/net/bteuk/uk121/world/gen/surfacedecoration/geojson directory

--- src/main/java/net/bteuk/uk121/world/gen/surfacedecoration/geojson/Property.java
+++
@@ -1,6 +0,0 @@
-package net.bteuk.uk121.world.gen.surfacedecoration.geojson;
-
-public class Property
-{
-    String[] property;
-}
--- src/main/java/net/bteuk/uk121/world/gen/surfacedecoration/geojson/Road.java
+++
@@ -1,6 +0,0 @@
-package net.bteuk.uk121.world.gen.surfacedecoration.geojson;
-
-public enum Road
-{
-    
-}
