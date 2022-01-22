0feb90d9e113fdc8f0329391120f5e6031c28f04
george112n <georgewalk168@gmail.com>
2022-01-22 15:02:07 +0000

    Added coastline, rail and highway:steps handling

--- src/main/java/net/bteuk/uk121/world/gen/surfacedecoration/UseType.java
+++ src/main/java/net/bteuk/uk121/world/gen/surfacedecoration/UseType.java
@@ -3,5 +3,6 @@ package net.bteuk.uk121.world.gen.surfacedecoration;
 public enum UseType
 {
     Water, Beach, Land, BuildingOutline, Motorway, MotorwayDerived, Primary, PrimaryDerived,
-    Secondary, SecondaryDerived, Tertiary, TertiaryDerived, Track, TrackDerived, Footway, FootwayDerived
+    Secondary, SecondaryDerived, Tertiary, TertiaryDerived, Track, TrackDerived, Footway, FootwayDerived,
+    Railway, xAlignedRail, yAlignedRail, railwaySleeper,
 }
